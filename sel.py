2445ea2755368a53b2a536520c9be8c3b24d5dad
Priyank Pulumati <pulumati.priyank@gmail.com>
2022-07-03 19:24:01 +0530

    refactored code in sel.py

--- sel.py
+++ sel.py
@@ -34,16 +34,12 @@ def get_text_from_captcha(driver, img_path):
     print(text)
     img_xpath = '/html/body/div[1]/div/div[1]/div[2]/div/div[2]/span/div/div[1]/a/img'
     if match is None:
-        # WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
-        #     (By.XPATH, img_xpath))).click()
         selenium_click_xpath(driver, img_xpath)
         get_captcha(driver)
         return get_text_from_captcha(driver, img_path)
     else:
         print(match.group(1))
         if (len(match.group(1)) != 6):
-            # WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
-            #     (By.XPATH, img_xpath))).click()
             selenium_click_xpath(driver, img_xpath)
             get_captcha(driver)
             return get_text_from_captcha(driver, img_path)
@@ -72,27 +68,17 @@ def main():
     driver = webdriver.Firefox(service=Service(
         GeckoDriverManager().install()), options=options)
     driver.get('https://hcservices.ecourts.gov.in/hcservices/main.php')
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
-        (By.XPATH, '//*[@id="cino"]'))).send_keys("HBHC010549632021")
+    selenium_send_keys_xpath(driver, '//*[@id="cino"]', "HBHC010549632021")
     is_failed_with_captach = True
     while is_failed_with_captach:
         get_captcha(driver)
         text = get_text_from_captcha(driver, r"image.png")
-        # WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
-        #     (By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div/div[2]/span/div/div[2]/label"))).click()
         selenium_click_xpath(
             driver, "/html/body/div[1]/div/div[1]/div[2]/div/div[2]/span/div/div[2]/label")
-        # WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
-        #     (By.XPATH, '//*[@id="captcha"]'))).send_keys(text)
         selenium_send_keys_xpath(driver, '//*[@id="captcha"]', text)
-
-        # WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
-        #     (By.XPATH, '//*[@id="searchbtn"]'))).click()
         selenium_click_xpath(driver, '//*[@id="searchbtn"]')
 
         try:
-            # failure_text = WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
-            #     (By.XPATH, '//*[@id="errSpan"]/p'))).text
             failure_text = selenium_get_text_xpath(
                 driver, '//*[@id="errSpan"]/p')
             if failure_text == 'THERE IS AN ERROR':
