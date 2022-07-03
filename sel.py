bea55fd3175011a27e6f8ffeaa576ce69d36d32e
Priyank Pulumati <pulumati.priyank@gmail.com>
2022-07-03 22:33:39 +0530

    some more enhancements and json pretty printing

--- sel.py
+++ sel.py
@@ -9,7 +9,12 @@ from webdriver_manager.firefox import GeckoDriverManager
 import base64
 from pytesseract import pytesseract
 from PIL import Image
+import os
 import re
+import json
+import webbrowser
+import traceback
+import datetime
 
 
 def selenium_click_xpath(driver, xpath):
@@ -25,6 +30,20 @@ def selenium_send_keys_xpath(driver, xpath, text):
 def selenium_get_text_xpath(driver, xpath):
     return WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
         (By.XPATH, xpath))).text
+
+
+def selenium_get_element_xpath(driver, xpath):
+    return WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+        (By.XPATH, xpath)))
+
+
+def get_table_data_as_list(driver, xpath):
+    rows = []
+    table = driver.find_element(by="xpath", value=xpath)
+    for row in table.find_elements(by="xpath", value='.//tr'):
+        rows.append(
+            [td.text for td in row.find_elements(by="xpath", value=".//td")])
+    return rows
 
 
 def get_text_from_captcha(driver, img_path):
@@ -67,33 +86,106 @@ def main():
     options.headless = True
     driver = webdriver.Firefox(service=Service(
         GeckoDriverManager().install()), options=options)
-    driver.get('https://hcservices.ecourts.gov.in/hcservices/main.php')
-    selenium_send_keys_xpath(driver, '//*[@id="cino"]', "HBHC010549632021")
+    # case_no = 'HBHC010549632021'
+    case_no = 'HBHC010557542021'
     is_failed_with_captach = True
     while is_failed_with_captach:
+        driver.get('https://hcservices.ecourts.gov.in/hcservices/main.php')
+        selenium_send_keys_xpath(driver, '//*[@id="cino"]', case_no)
         get_captcha(driver)
         text = get_text_from_captcha(driver, r"image.png")
         selenium_click_xpath(
             driver, "/html/body/div[1]/div/div[1]/div[2]/div/div[2]/span/div/div[2]/label")
         selenium_send_keys_xpath(driver, '//*[@id="captcha"]', text)
         selenium_click_xpath(driver, '//*[@id="searchbtn"]')
-
+        is_failed_with_captach = False
         try:
             failure_text = selenium_get_text_xpath(
-                driver, '//*[@id="errSpan"]/p')
-            if failure_text == 'THERE IS AN ERROR':
-                is_failed_with_captach = True
+                driver, '//*[@id="errSpan"]')
+            print(failure_text)
+            if 'THERE IS AN ERROR' in failure_text:
+                print("in first")
+                is_failed_with_captach = False
         except:
-            is_failed_with_captach = False
-            case_title = selenium_get_text_xpath(
-                driver, '/html/body/div[1]/div/div[1]/div[2]/div/div[2]/div[52]/div[2]/div[1]/div/table/tbody/tr[1]/td[2]')
-            print(case_title)
+            try:
+                failure_text_other_page = selenium_get_text_xpath(
+                    driver, '/html/body/div[1]/div/div[1]/div[2]/div/div[2]/div[52]/p')
+                print(failure_text_other_page)
+                if "invalid" in failure_text_other_page.lower():
+                    selenium_click_xpath(driver, '//*[@id="bckbtn"]')
+                    is_failed_with_captach = True
+                else:
+                    print("in second")
+                    is_failed_with_captach = False
+            except:
+                pass
+    # case details
+    case_details_title = selenium_get_text_xpath(
+        driver, '/html/body/div[1]/div/div[1]/div[2]/div/div[2]/div[52]/div[2]/div[1]/div/table/tbody/tr[1]/td[2]')
+    case_details_registration_no = selenium_get_text_xpath(
+        driver, '//*[@id="caseBusinessDiv4"]/div/table/tbody/tr[2]/td[2]/label')
+    case_details_cnr_no = selenium_get_text_xpath(
+        driver, '//*[@id="caseBusinessDiv4"]/div/table/tbody/tr[3]/td[2]/strong')
+    case_details_filing_date = selenium_get_text_xpath(
+        driver, '//*[@id="caseBusinessDiv4"]/div/table/tbody/tr[1]/td[4]')
+    case_details_registration_date = selenium_get_text_xpath(
+        driver, '//*[@id="caseBusinessDiv4"]/div/table/tbody/tr[2]/td[4]/label')
+    # case status
+    case_status = get_table_data_as_list(
+        driver, '//*[@id="caseBusinessDiv4"]/table')
+    # paa = petitioned and advocate
+    case_paa = selenium_get_text_xpath(
+        driver, '//*[@id="caseHistoryDiv"]/div[2]/div[2]/span[1]')
+    # raa = respondent and advocate
+    case_raa = selenium_get_text_xpath(
+        driver, '//*[@id="caseHistoryDiv"]/div[2]/div[2]/span[2]')
+    # acts
+    case_acts = get_table_data_as_list(driver, '//*[@id="act_table"]')
+    # history
+    case_history = get_table_data_as_list(
+        driver, '//*[@id="caseHistoryDiv"]/div[2]/div[2]/table[2]')
+    # orders
+    case_orders = get_table_data_as_list(
+        driver, '//*[@id="caseHistoryDiv"]/div[2]/div[2]/table[4]')
+    # objections
+    case_objections = get_table_data_as_list(
+        driver, '//*[@id="caseHistoryDiv"]/div[3]/table')
+    data = {
+        "title": case_details_title,
+        "registration_no": case_details_registration_no,
+        "cnr_no": case_details_cnr_no,
+        "filing_date": case_details_filing_date,
+        "registration_date": case_details_registration_date,
+        "status": case_status,
+        "paa": case_paa,
+        "raa": case_raa,
+        "acts": case_acts,
+        "history": case_history,
+        "orders": case_orders,
+        "objections": case_objections,
+    }
+    print(data)
+    page = "scrape.html"
+    with open(page, "w+", newline="", encoding="UTF-8") as f:
+        f.write("<html><body><pre id='json'></pre></body></html>")
+        f.write("<script>")
+        f.write(
+            f"document.getElementById('json').textContent = JSON.stringify({json.dumps(data)}, undefined, 2);")
+        f.write("</script>")
+    webbrowser.open('file://' + os.path.realpath(page), new=2)
+
     driver.close()
     driver.quit()
 
 
 if __name__ == "__main__":
     try:
+        start = datetime.datetime.now()
         main()
     except Exception as e:
-        print(e.__str__())
+        print(traceback.format_exc())
+        print(str(e))
+    finally:
+        end = datetime.datetime.now()
+        total = end - start
+        print(total.seconds)
