299f496ea07e8e5f517d74ab7cf1db2ea75a9353
Chetna Singh <chetnasingh023@gmail.com>
2022-09-19 09:53:48 +0530

    Test download (#1)
    * Adds usecase1 feature
    
    * Test download pdf and save
    
    * added ability to download pdf from a case
    
    * adds chrome option
    
    Co-authored-by: Priyank Pulumati <pulumati.priyank@gmail.com>

--- name.py
+++ name.py
@@ -1,25 +1,26 @@
+# from selenium.webdriver.firefox.service import Service
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
-from time import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.support.ui import Select
+
 
 import base64
-import cv2
-import datetime
-import easyocr
-import json
 import os
 import re
-import tempfile
-import time
-import traceback
+import json
 import webbrowser
+import traceback
+import datetime
+import cv2
+import easyocr
+import time
+
+
 
 # get grayscale image
 def get_grayscale(image):
@@ -36,8 +37,13 @@ def selenium_click_xpath(driver, xpath):
 
 
 def selenium_send_keys_xpath(driver, xpath, text):
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+    WebDriverWait(driver, 40).until(EC.element_to_be_clickable(
         (By.XPATH, xpath))).send_keys(text)
+
+
+def selenium_send_keys_id(driver, id, text):
+    WebDriverWait(driver, 40).until(EC.element_to_be_clickable(
+        (By.ID, id))).send_keys(text)
 
 
 def selenium_get_text_xpath(driver, xpath):
@@ -48,6 +54,23 @@ def selenium_get_text_xpath(driver, xpath):
 def selenium_get_element_xpath(driver, xpath):
     return WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
         (By.XPATH, xpath)))
+
+def selenium_get_element_id(driver, id):
+    return WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+        (By.ID, id)))
+
+def selenium_find_element_css_selector(driver, selector):
+    return WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, selector)))
+
+def selenium_click_id(driver, id):
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+        (By.ID, id))).click()
+    
+def selenium_click_class(driver, classname):
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+        (By.CLASS_NAME, classname))).click()
+
 
 
 def get_table_data_as_list(driver, xpath):
@@ -64,7 +87,7 @@ def get_text_from_captcha(driver, img_path):
     result = reader.readtext(img_path)
     match = re.search(r'\(?([0-9A-Za-z]+)\)?', result[0][1])
     print(result[0][1])
-    img_xpath = '/html/body/div[1]/div/div[1]/div[2]/div/div[2]/span/div/div[1]/a/img'
+    img_xpath = '/html/body/div[1]/div/div[1]/div[2]/div/div[2]/span/div/div[1]/div[1]/img'
     if match is None:
         selenium_click_xpath(driver, img_xpath)
         get_captcha(driver)
@@ -93,37 +116,53 @@ def get_captcha(driver):
         f.write(base64.b64decode(img_base64))
 
 
+
 def main():
     options = Options()
     # options.add_argument("--headless")
     # options.headless = True
-    profile = FirefoxProfile()
-    profile.set_preference(
-        "browser.helperApps.neverAsk.saveToDisk", "application/octet-stream;application/vnd.ms-excel;text/html;application/pdf")
-    profile.set_preference('pdfjs.disabled', True)
-    profile.set_preference('print.always_print_silent', True)
-    profile.set_preference('print.show_print_progress', False)
-    profile.set_preference('browser.download.show_plugins_in_list', False)
-    profile.set_preference('browser.download.folderList', 2)
-    driver = webdriver.Firefox(service=Service(
-        GeckoDriverManager().install()), options=options, firefox_profile=profile)
-    actions = ActionChains(driver)
-    # case_no = 'HBHC010549632021'
-    case_no = 'HBHC010557542021'
+    # driver = webdriver.Chrome(service=Service(
+    #     GeckoDriverManager().install()), options=options)
+    DRIVER_PATH = '/usr/local/bin/chromedriver'
+    driver = webdriver.Chrome(DRIVER_PATH)
+    # driver = webdriver.Chrome()
+    service = Service(DRIVER_PATH)
+
+    service.start()
+    driver = webdriver.Remote(service.service_url)
+    advoc_name='V Aneesh'
+    sess_state_code='High Court for State of Telangana'
+    court_complex_code='Principal Bench at Hyderabad'
     is_failed_with_captach = True
     while is_failed_with_captach:
         driver.get('https://hcservices.ecourts.gov.in/hcservices/main.php')
-        selenium_send_keys_xpath(driver, '//*[@id="cino"]', case_no)
+        selenium_click_id(driver,'leftPaneMenuCS')
+        print("Successfully clicked")
+        time.sleep(5)
+        selenium_click_xpath(driver,'/html/body/div[2]/div/div/div[2]/button')
+        print("ok clicked")
+        time.sleep(5)
+        state_code= Select(selenium_get_element_id(driver ,'sess_state_code'))
+        state_code.select_by_value('29')
+        print("Values selected")
+        time.sleep(3)
+        court_code=Select(selenium_get_element_id(driver ,'court_complex_code'))
+        court_code.select_by_value('1')
+        print("court code selected")
+        selenium_click_id(driver,'CSAdvName')
+        print("hypelink clicked")
+        selenium_send_keys_xpath(driver, '/html/body/div[1]/div/div[1]/div[2]/div/div[2]/div[14]/div[2]/div[2]/input', advoc_name)
+        print("names sent")
         get_captcha(driver)
         text = get_text_from_captcha(driver, r"image.png")
         selenium_click_xpath(
-            driver, "/html/body/div[1]/div/div[1]/div[2]/div/div[2]/span/div/div[2]/label")
+        driver, "/html/body/div[1]/div/div[1]/div[2]/div/div[2]/span/div/div[2]/label")  
         selenium_send_keys_xpath(driver, '//*[@id="captcha"]', text)
-        selenium_click_xpath(driver, '//*[@id="searchbtn"]')
+        selenium_click_xpath(driver, '//*[@class="Gobtn"]')
         is_failed_with_captach = False
         try:
             failure_text = selenium_get_text_xpath(
-                driver, '//*[@id="errSpan"]')
+                driver, '//*[@id="errSpan1"]')
             print(failure_text)
             if 'THERE IS AN ERROR' in failure_text:
                 print("in first")
@@ -131,7 +170,7 @@ def main():
         except:
             try:
                 failure_text_other_page = selenium_get_text_xpath(
-                    driver, '/html/body/div[1]/div/div[1]/div[2]/div/div[2]/div[52]/p')
+                    driver, '/html/body/div[1]/div/div[1]/div[2]/div/div[1]/ul/li[2]/div/p')
                 print(failure_text_other_page)
                 if "invalid" in failure_text_other_page.lower():
                     selenium_click_xpath(driver, '//*[@id="bckbtn"]')
@@ -141,6 +180,20 @@ def main():
                     is_failed_with_captach = False
             except:
                 pass
+    # case details
+    number_of_establishments_in_court_complex= selenium_get_text_xpath(
+        driver, '//*[@id="showList2"]/div[1]/h3')
+    print(number_of_establishments_in_court_complex)
+    number_of_cases= selenium_get_text_xpath(
+        driver, '//*[@id="showList2"]/div[1]/h4')
+    print(number_of_cases)
+    #list of case 
+    case_list= get_table_data_as_list(
+        driver, '/html/body/div[1]/div/div[1]/div[2]/div/div[2]/div[45]/table')
+    #click for the view hyperlink
+    selenium_click_class(driver,'someclass')
+    print("view clicked")
+    #details behind the hypelink
     # case details
     case_details_title = selenium_get_text_xpath(
         driver, '/html/body/div[1]/div/div[1]/div[2]/div/div[2]/div[52]/div[2]/div[1]/div/table/tbody/tr[1]/td[2]')
@@ -169,26 +222,14 @@ def main():
     # orders
     case_orders = get_table_data_as_list(
         driver, '//*[@id="caseHistoryDiv"]/div[2]/div[2]/table[4]')
-    print(case_orders)
-    case_orders_element = driver.find_element(by="xpath",
-                                            value='//*[@id="caseHistoryDiv"]/div[2]/div[2]/table[4]')
-
-    tmp_file = os.path.realpath('orderDetails.pdf')
-    print ("tmp_file=======", tmp_file)
-    driver.set_window_position(0, 0)
-    driver.set_window_size(1024, 768)
-    profile.set_preference('browser.download.dir', tmp_file)
-    time.sleep(2)
-    driver.execute_script(
-    "arguments[0].scrollIntoView();", case_orders_element)
-    time.sleep(2)
-    selenium_click_xpath(driver,
-                    '/html/body/div[1]/div/div[1]/div[2]/div/div[2]/div[52]/div[2]/div[2]/table[4]/tbody/tr[2]/td[5]/a')
-
     # objections
     case_objections = get_table_data_as_list(
         driver, '//*[@id="caseHistoryDiv"]/div[3]/table')
     data = {
+        "number_of_establishments_in_court_complex":number_of_establishments_in_court_complex,
+        "number_of_cases":number_of_cases,
+        "case_list":case_list,
+        "case_details":{
         "title": case_details_title,
         "registration_no": case_details_registration_no,
         "cnr_no": case_details_cnr_no,
@@ -201,9 +242,12 @@ def main():
         "history": case_history,
         "orders": case_orders,
         "objections": case_objections,
+        },
+        
+
     }
     print(data)
-    page = "scrape.html"
+    page = "scrape1.html"
     with open(page, "w+", newline="", encoding="UTF-8") as f:
         f.write("<html><body><pre id='json'></pre></body></html>")
         f.write("<script>")
@@ -214,7 +258,6 @@ def main():
 
     driver.close()
     driver.quit()
-
 
 
 if __name__ == "__main__":
