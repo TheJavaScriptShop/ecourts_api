dba3addd220c020156063c5d41b8ab3e73323aea
Priyank Pulumati <pulumati.priyank@gmail.com>
2022-09-26 10:55:40 +0530

    refactor part 9

--- WebHandler/__init__.py
+++ WebHandler/__init__.py
@@ -2,6 +2,8 @@ import logging
 import os
 import json
 import time
+import requests
+import threading
 
 import azure.functions as func
 from selenium import webdriver
@@ -11,6 +13,13 @@ from .scrappers.highcourts import get_highcourt_cases_by_name
 
 __location__ = os.path.realpath(os.path.join(
     os.getcwd(), os.path.dirname(__file__)))
+
+
+def fire_and_forget(f):
+    def wrapped():
+        threading.Thread(target=f).start()
+
+    return wrapped
 
 
 def main_handler(req: func.HttpRequest) -> func.HttpResponse:
@@ -43,6 +52,8 @@ def main_handler(req: func.HttpRequest) -> func.HttpResponse:
             is_valid_request = False
         if not req_body.get("benchCode"):
             is_valid_request = False
+        if not req_body.get("callBackUrl"):
+            is_valid_request = False
     else:
         return func.HttpResponse(
             body=json.dumps(
@@ -67,9 +78,20 @@ def main_handler(req: func.HttpRequest) -> func.HttpResponse:
         "/usr/local/bin/chromedriver", chrome_options=chrome_options)
 
     if req_params.get("method") == "advocatecasesbyname":
-        data = get_highcourt_cases_by_name(driver, req_body.get(
-            "advocateName"), req_body.get("highCourtId"), req_body.get("benchCode"))
-        return func.HttpResponse(json.dumps({"status": data["status"], "data": data["data"], "request": {"body": req_body, "params": req_params}}))
+        @fire_and_forget
+        def get_highcourt_cases_by_name_wrapper():
+            data = get_highcourt_cases_by_name(driver, req_body.get(
+                "advocateName"), req_body.get("highCourtId"), req_body.get("benchCode"))
+            try:
+                requests.post(url=req_body.get("callBackUrl"), timeout=10, json=json.dumps(
+                    {"status": data["status"], "data": data["data"], "request": {"body": req_body, "params": req_params}}))
+            except:
+                pass
+        get_highcourt_cases_by_name_wrapper()
+        return func.HttpResponse(
+            body=json.dumps(
+                {"status": True, "debugMessage": "Request Received and processing", "request": {"body": req_body, "params": req_params}}),
+        )
     return func.HttpResponse(
         body=json.dumps(
             {"status": False, "debugMessage": "Method not supported", "request": {"body": req_body, "params": req_params}}),
