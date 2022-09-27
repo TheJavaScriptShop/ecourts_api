fa271619c03df7d3a58a82d6f7dadc469c702c17
Sarvani Sanaboyina <sarvani@thejavascriptshop.com>
2022-09-27 15:29:14 +0530

    remove comments

--- adv_name.py
+++ adv_name.py
@@ -166,36 +166,29 @@ def main(advoc_name, high_court_id, bench_id):
     fetched_data = False
 
     def wait_for_download_and_rename(newFilename):
-        # function to wait for all chrome downloads to finish
         def chrome_downloads(drv):
-            if not "chrome://downloads" in drv.current_url: # if 'chrome downloads' is not current tab
-                drv.execute_script("window.open('');") # open a new tab
-                drv.switch_to.window(driver.window_handles[1]) # switch to the new tab
-                drv.get("chrome://downloads/") # navigate to chrome downloads
+            if not "chrome://downloads" in drv.current_url: 
+                drv.execute_script("window.open('');") 
+                drv.switch_to.window(driver.window_handles[1]) 
+                drv.get("chrome://downloads/") 
             return drv.execute_script("""
                 return document.querySelector('downloads-manager')
                 .shadowRoot.querySelector('#downloadsList')
                 .items.filter(e => e.state === 'COMPLETE')
                 .map(e => e.filePath || e.file_path || e.fileUrl || e.file_url);
                 """)
-        # wait for all the downloads to be completed
-        dld_file_paths = WebDriverWait(driver, 120, 1).until(chrome_downloads) # returns list of downloaded file paths
-        # Close the current tab (chrome downloads)
+        dld_file_paths = WebDriverWait(driver, 120, 1).until(chrome_downloads) 
         if "chrome://downloads" in driver.current_url:
             driver.close()
-        # Switch back to original tab
         driver.switch_to.window(driver.window_handles[0]) 
-        # get latest downloaded file name and path
-        dlFilename = dld_file_paths[0] # latest downloaded file from the list
-        # wait till downloaded file appears in download directory
-        time_to_wait = 20 # adjust timeout as per your needs
+        dlFilename = dld_file_paths[0] 
+        time_to_wait = 20 
         time_counter = 0
         while not os.path.isfile(dlFilename):
             time.sleep(1)
             time_counter += 1
             if time_counter > time_to_wait:
                 break
-        # rename the downloaded file
         shutil.move(dlFilename, os.path.join(file_path,newFilename))
         return
 
