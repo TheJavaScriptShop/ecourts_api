b52fd004ab5dd48c24684c32a87dc01d3dd3e6bd
Sarvani Sanaboyina <sarvani@thejavascriptshop.com>
2022-10-17 13:37:06 +0530

    debug 2

--- WebHandler/utils/sel.py
+++ WebHandler/utils/sel.py
@@ -88,19 +88,20 @@ def get_display_board_table_data_as_list(driver, xpath):
 
 
 def get_cause_list_table_data_as_list(driver, xpath):
-    rows = []
+    table_data = []
     table = driver.find_element(by="xpath", value=xpath)
     for tbody in table.find_elements(by="xpath", value='.//tbody'):
-        if tbody.find_elements(by="xpath", value=".//td"):
-            rows.append(
-                [td.text for td in tbody.find_elements(by="xpath", value=".//td")])
-        if tbody.find_elements(by="xpath", value=".//th"):
-            for row in table.find_elements(by="xpath", value='.//tr'):
+        rows = []
+        for row in tbody.find_elements(by="xpath", value='.//tr'):
+            if row.find_elements(by="xpath", value=".//td"):
+                rows.append(
+                    [td.text for td in tbody.find_elements(by="xpath", value=".//td")])
+            if row.find_elements(by="xpath", value=".//th"):
                 if len(row.find_elements(by="xpath", value=".//th")) > 1:
                     rows.append(
                         [th.text for th in row.find_elements(by="xpath", value=".//th")])
                 else:
                     for th in row.find_elements(by="xpath", value=".//th"):
                         rows.append(th.text)
-
-    return rows
+        table_data.append(rows)
+    return table_data
