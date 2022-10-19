0e7521490e8ab61661981924afd4010bca7e4e32
Sarvani Sanaboyina <sarvani@thejavascriptshop.com>
2022-10-19 22:25:39 +0530

    handle case: session ended

--- WebHandler/scrappers/display_board.py
+++ WebHandler/scrappers/display_board.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import Select
 from azure.storage.blob import BlobServiceClient
 from dotenv import load_dotenv
 
-from ..utils.sel import get_display_board_table_data_as_list
+from ..utils.sel import get_display_board_table_data_as_list, selenium_get_text_xpath
 import WebHandler.scrappers.constants as constants
 
 import time
@@ -22,6 +22,8 @@ def get_display_board(driver, highCourtId):
             return {"status": False, "message": "Invalid request"}
         driver.get(url)
         time.sleep(3)
+        if "SESSION ENDED" in selenium_get_text_xpath(driver, '/html/body/div[2]/form/center/table/tbody/tr/td/h1/font'):
+            return {"message": "SESSION ENDED"}
         data = get_display_board_table_data_as_list(
             driver, "//table[@id='table1']")
         return data
